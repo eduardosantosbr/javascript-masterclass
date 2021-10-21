5d4bf94cac7877cb53de6ddf0ea741f173971031
Eduardo Nunes Santos <eduardo.ns.nunes@gmail.com>
2021-10-20 23:08:58 -0300

    add createIterable

--- iterables-and-iterators/example6.js
+++ iterables-and-iterators/example6.js
@@ -1,21 +1,30 @@
-function createIterator(...array) {
-    let i = 0;
+function createIterable(...array) {
     return {
-        next() {
-            if (i < array.length) {
-                return {
-                    value: array[i++],
-                    done: false,
+        [Symbol.iterator]() {
+            let i = 0;
+            return {
+                next() {
+                    if (i < array.length) {
+                        return {
+                            value: array[i++],
+                            done: false,
+                        }
+                    } else {
+                        return {
+                            value: undefined,
+                            done: true,
+                        }
+                    }
                 }
-            } else {
-                return {
-                    value: undefined,
-                    done: true,
-                }
-            }
+            };
         }
-    };
+    }
 }
 
-const iterator = createIterator("Fortran", "Lisp", "COBOL");
-console.log(iterator.next());
+const iterable = createIterable("Fortran", "Lisp", "COBOL");
+
+for (const language of iterable) {
+    console.log(language);
+}
+
+console.log([...iterable]);

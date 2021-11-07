9fd75e77ff2e00baa8cd9546f156777dcfef4ce5
Eduardo Nunes Santos <eduardo.ns.nunes@gmail.com>
2021-11-07 20:53:06 -0300

    add all falsey values in JavaScript

---
+++ boolean/example1.js
@@ -0,0 +1,11 @@
+/**
+ * There are only six falsey values in JavaScript:
+ * undefined, null, NaN, 0, "", and false of course.
+ */
+
+console.log(`undefined: ${!!undefined}`);
+console.log(`null: ${!!null}`);
+console.log(`NaN: ${!!NaN}`);
+console.log(`0: ${!!0}`);
+console.log(`""(empty string): ${!!""}`);
+console.log(`false: ${!!false}`);

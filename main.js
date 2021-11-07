0e2e46c7a1f29e7c50ad77a5d809abf9339820c5
Eduardo Nunes Santos <eduardo.ns.nunes@gmail.com>
2021-11-07 19:52:13 -0300

    add async and await in main file

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 import { Database } from "./database.js";
 
-const database = new Database();
-database.execute("create table author (id number, name string, age number, city string, state string, country string)").then(function () {
-    return Promise.all([
-        database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
-        database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
-        database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)")
-    ]).then(function () {
-        return database.execute("select name, age from author").then(function (result) {
-            console.log(JSON.stringify(result, undefined, 2));
-        });
-    });
-}).catch(function (e) {
-    console.log(e.message);
-});
+(async function () {
+    try {
+        const database = new Database();
+        await database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+        await Promise.all([
+            database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+            database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+            database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)")
+        ]);
+        let result = await database.execute("select name, age from author");
+        console.log(JSON.stringify(result, undefined, 2));
+    } catch (e) {
+        console.log(e.message);
+    }
+})();
